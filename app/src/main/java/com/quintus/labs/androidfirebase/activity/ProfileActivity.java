7d70d97805c18d15277b57ca04820b05ebaadea7
Santosh Kumar Dash <santoshnet2016@gmail.com>
2021-03-19 15:11:04 +0530

    profile updated

--- app/src/main/java/com/quintus/labs/androidfirebase/activity/ProfileActivity.java
+++ app/src/main/java/com/quintus/labs/androidfirebase/activity/ProfileActivity.java
@@ -3,17 +3,22 @@ package com.quintus.labs.androidfirebase.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -27,7 +32,7 @@ import com.quintus.labs.androidfirebase.model.User;
 public class ProfileActivity extends AppCompatActivity {
     private static final String TAG = "ProfileActivity=>";
     private Button btnChangeEmail, btnChangePassword, btnSendResetEmail, btnRemoveUser,
-            changeEmail, changePassword, sendEmail, remove, signOut;
+            changeEmail, changePassword, sendEmail, remove, signOut,updateUser;
 
     private EditText oldEmail, newEmail, password, newPassword;
     FirebaseUser firebaseUser;
@@ -37,7 +42,7 @@ public class ProfileActivity extends AppCompatActivity {
     User user;
     TextView name,email,phone,gender,address;
     private FirebaseAuth auth;
-
+    ImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,6 +66,7 @@ public class ProfileActivity extends AppCompatActivity {
         sendEmail = (Button) findViewById(R.id.send);
         remove = (Button) findViewById(R.id.remove);
         signOut = (Button) findViewById(R.id.sign_out);
+        updateUser = (Button) findViewById(R.id.update_user_button);
 
         oldEmail = (EditText) findViewById(R.id.old_email);
         newEmail = (EditText) findViewById(R.id.new_email);
@@ -94,21 +100,37 @@ public class ProfileActivity extends AppCompatActivity {
         changeEmail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 if (firebaseUser != null && !newEmail.getText().toString().trim().equals("")) {
                     firebaseUser.updateEmail(newEmail.getText().toString().trim())
                             .addOnCompleteListener(new OnCompleteListener<Void>() {
                                 @Override
                                 public void onComplete(@NonNull Task<Void> task) {
                                     if (task.isSuccessful()) {
-                                        Toast.makeText(getApplicationContext(), "Email address is updated. Please sign in with new email id!", Toast.LENGTH_LONG).show();
+                                         user.setEmail(newEmail.getText().toString().trim());
+                                        DocumentReference documentReference = db.collection("users").document(firebaseUser.getUid());
+                                        documentReference
+                                                .update("email", newEmail.getText().toString().trim())
+                                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                    @Override
+                                                    public void onSuccess(Void aVoid) {
+                                                        Log.d(TAG, "DocumentSnapshot successfully updated!");
+                                                    }
+                                                })
+                                                .addOnFailureListener(new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+                                                        Log.w(TAG, "Error updating document", e);
+                                                    }
+                                                });
+
+                                        Toast.makeText(getApplicationContext(), "Email address updated successfull", Toast.LENGTH_SHORT).show();
                                         signOut();
-                                    } else {
-                                        Toast.makeText(getApplicationContext(), "Failed to update email!", Toast.LENGTH_LONG).show();
+                                    }else{
+                                        Log.d(TAG, "User email address Failed.");
                                     }
                                 }
                             });
-                } else if (newEmail.getText().toString().trim().equals("")) {
-                    newEmail.setError("Enter email");
                 }
             }
         });
@@ -171,17 +193,18 @@ public class ProfileActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (!oldEmail.getText().toString().trim().equals("")) {
-                    auth.sendPasswordResetEmail(oldEmail.getText().toString().trim())
-                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    if (task.isSuccessful()) {
-                                        Toast.makeText(getApplicationContext(), "Reset password email is sent!", Toast.LENGTH_SHORT).show();
-                                    } else {
-                                        Toast.makeText(getApplicationContext(), "Failed to send reset email!", Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-                            });
+                    auth.sendPasswordResetEmail(oldEmail.getText().toString().trim()) .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(), "We have sent you instructions to reset your password!", Toast.LENGTH_SHORT).show();
+
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Failed to send reset email!", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
                 } else {
                     oldEmail.setError("Enter email");
 
@@ -217,7 +240,41 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+        updateUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create custom dialog object
+                final Dialog dialog = new Dialog(ProfileActivity.this,R.style.theme_sms_receive_dialog);
+                Window window = dialog.getWindow();
+                window.setLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+                // Include dialog.xml file
+                dialog.setContentView(R.layout.update_user);
+                // Set dialog title
+                dialog.setTitle("Update User");
 
+                // set values for custom dialog components - text, image and button
+                EditText name = (EditText) dialog.findViewById(R.id.name);
+                EditText phone = (EditText) dialog.findViewById(R.id.phone);
+                EditText gender = (EditText) dialog.findViewById(R.id.gender);
+                EditText address = (EditText) dialog.findViewById(R.id.address);
+                image = (ImageView) dialog.findViewById(R.id.image_view);
+                dialog.show();
+
+                name.setText(user.getName());
+                phone.setText(user.getPhone());
+                gender.setText(user.getGender());
+                address.setText(user.getAddress());
+
+                image.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                });
+
+
+            }
+        });
 
     }
 
@@ -246,7 +303,7 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
     private void getFirebaseUser() {
-        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        firebaseUser = auth.getCurrentUser();
     }
 
 }
